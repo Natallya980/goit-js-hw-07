778e50a0ad4398d9eff59e308a7fd847da88404e
NikkyBG <nikkybg57@gmail.com>
2025-07-06 21:23:12 +0300

    add file

--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,7 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value += str;
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-}
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
+inputEl.addEventListener("input", () => {
+  const trimmedValue = inputEl.value.trim();
+  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+  alue;
+});
